3c7fd05021d2577e24ef03dffdee13301bda0cb5
Christine Zheng <christinezheng@Meltons-MacBook-Pro.local>
2022-02-02 18:39:43 -0800

    initial yelp api

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ fetch("https://yelp-graphql1.p.rapidapi.com/v3/graphql", {
 	"headers": {
 		"authorization": "undefined",
 		"x-rapidapi-host": "yelp-graphql1.p.rapidapi.com",
-		"x-rapidapi-key":"KNsw6f40rp6KOffo_dCp8eigMhvJoLHN6AtO6ck5fSBB6_w12JDEdgaZo6_wiEyhyd1Y54ge3LdmdKA754oeR14rjKE2_PzZwnWmtj5VPk0nl1iTxNBhKDdrerv4YXYx"
+		"x-rapidapi-key":"
 	}
 })
 .then(response => {
