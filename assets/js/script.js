cc33eb526faa4ec7f4b110f28389523cec068c69
Christine Zheng <christinezheng@Meltons-MacBook-Pro.local>
2022-02-03 19:33:49 -0800

    yelp api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
-var key = config.secretKey;
-fetch("https://api.yelp.com/v3/businesses/search?location=NYC", {
+fetch("https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=NYC", {
   "method": "GET",
   "headers": {
-    "Authorization": "Bearer " + key
+    "Authorization": "Bearer ",
   }
+})
+.then(response => {
+  return response.json();
 })
 .then(response => {
   console.log(response);
