296c1de498d23e143bd879c352299b72e2a5e018
Justyn Subrai <justynskylarsubrai@gmail.com>
2022-02-06 17:21:17 -0800

    created map tiles

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,9 +11,21 @@ function thing() {
     return response.json();
   })
   .then(response => {
+    let lat = response.businesses[0].coordinates.latitude
+    let long = response.businesses[0].coordinates.longitude
+    let map = L.map('map').setView([lat, long], 20);
 
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'pk.eyJ1IjoianN1YnJhaSIsImEiOiJja3piejB6czMyYTA1MnlwcjJmaXljdGNmIn0.Ukuvh6H-szz9m6G8cAamQQ'
+  }).addTo(map);
 
-    
+    console.log(lat)
+    console.log(long)
     console.log(response);
     document.querySelector("#testText").innerHTML = response.businesses[0].name
     document.querySelector("#testImage").setAttribute("src", response.businesses[0].image_url)
@@ -22,17 +34,18 @@ function thing() {
     console.error(err);
   });
 }
-// var key = process.env.key
 
 
 // Fetch opentripmap below
-
-// fetch ('https://api.opentripmap.com/0.1/en/places/geoname?name=Westwood&country=us&apikey=5ae2e3f221c38a28845f05b65aa94ae67c8a988c80831bd867503215') 
+// fetch ('https://api.opentripmap.com/0.1/­en/tiles/pois­/10­/1/1.pbf&apikey=5ae2e3f221c38a28845f05b65aa94ae67c8a988c80831bd867503215') 
 //     .then(response => response.json())
 //     .then(data => {
 
 
 
 //         console.log(data)
-//         console.log(data.lat)
+//         // console.log(data.lat)
 //     })
+
+
+// https://api.opentripmap.com/0.1/­{lang}­/tiles/{layer}­/{z}­/{x}­/{y}.pbf?kinds={kinds}&rate={rate}&apikey={apikey}
