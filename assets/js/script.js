a12c9935dbac59b9f66f12dcb5d576e38f644cc4
Christine Zheng <christinezheng@Meltons-MacBook-Pro.local>
2022-02-07 21:30:25 -0800

    map and for loop change

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,14 +28,11 @@ function thing() {
 }
 
 function displayMap(lat,long) {
-  // $(mapEL).html("");
 
-  let map = L.map('map').setView([lat, long], 20);
-  
-  // if (map!=undefined) {
-  //   map.off();
-  //  map.remove(); 
-  // };
+	document.getElementById('map-container').innerHTML = "<div id='map'></div>";
+	let map = new L.map('map');
+
+	map.setView([lat, long], 20);
 
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -49,12 +46,28 @@ function displayMap(lat,long) {
 };
 
 function displayShop(response) {
-
-  document.querySelector("#testText").innerHTML = response.businesses[0].name
-  document.querySelector("#testImage").setAttribute("src", response.businesses[0].image_url)
-  document.getElementById("address").textContent = response.businesses[0].location.display_address
-  document.querySelector("#cityName").textContent = response.businesses[0].location.city
-  document.getElementById('thing').value = "";
+var dataEl = (`<div class="flex-wrap">`);
+  for (let i=1; i<6; i++) {
+  var nameEl = response.businesses[i].name;
+  var imageUrlEl = response.businesses[i].image_url;
+  var addressEl = response.businesses[i].location.display_address;
+  var cityEl = response.businesses[i].location.city;
+  dataEl = dataEl + (`
+  <div>
+  <ul>
+      <li>${nameEl}</li>
+      <li>${addressEl}</li>
+      <li><img src="${imageUrlEl}"></li>
+      <li>${cityEl}</li>
+  </ul>
+  </div>`)
+}
+ $("#dataResponse").html(dataEl)
+//   document.querySelector("#testText").innerHTML = response.businesses[0].name
+//   document.querySelector("#testImage").setAttribute("src", response.businesses[0].image_url)
+//   document.getElementById("address").textContent = response.businesses[0].location.display_address
+//   document.querySelector("#cityName").textContent = response.businesses[0].location.city
+//   document.getElementById('thing').value = "";
 
 }
 
@@ -75,3 +88,51 @@ document.getElementById("thingBtn").addEventListener("click", thing);
 
 
 // https://api.opentripmap.com/0.1/­{lang}­/tiles/{layer}­/{z}­/{x}­/{y}.pbf?kinds={kinds}&rate={rate}&apikey={apikey}
+
+
+// Below isthe Modal that pops out an alert.
+document.addEventListener('DOMContentLoaded', () => {
+  // Functions to open and close a modal
+  function openModal($el) {
+    $el.classList.add('is-active');
+  }
+
+  function closeModal($el) {
+    $el.classList.remove('is-active');
+  }
+
+  function closeAllModals() {
+    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+      closeModal($modal);
+    });
+  }
+
+  // Add a click event on buttons to open a specific modal
+  (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+    const modal = $trigger.dataset.target;
+    const $target = document.getElementById(modal);
+    console.log($target);
+
+    $trigger.addEventListener('click', () => {
+      openModal($target);
+    });
+  });
+
+  // Add a click event on various child elements to close the parent modal
+  (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+    const $target = $close.closest('.modal');
+
+    $close.addEventListener('click', () => {
+      closeModal($target);
+    });
+  });
+
+  // Add a keyboard event to close all modals
+  document.addEventListener('keydown', (event) => {
+    const e = event || window.event;
+
+    if (e.keyCode === 27) { // Escape key
+      closeAllModals();
+    }
+  });
+});
