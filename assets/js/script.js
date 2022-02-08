a44779454ead26d9a1273235bc42d5eaaac4f31f
Christine Zheng <christinezheng@Meltons-MacBook-Pro.local>
2022-02-07 20:13:04 -0800

    push out for sandrine

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var mapEL = $("#map")
+
 function thing() {
   console.log(document.querySelector("#thing").value)
   fetch("https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=" + document.querySelector("#thing").value, {
@@ -12,32 +14,54 @@ function thing() {
   .then(response => {
     let lat = response.businesses[0].coordinates.latitude
     let long = response.businesses[0].coordinates.longitude
-    let map = L.map('map').setView([lat, long], 20);
+    displayMap(lat, long);
+    displayShop(response);
+    // console.log(lat)
+    // console.log(long)
+    // console.log(response);
 
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 18,
-      id: 'mapbox/streets-v11',
-      tileSize: 512,
-      zoomOffset: -1,
-      accessToken: 'pk.eyJ1IjoianN1YnJhaSIsImEiOiJja3piejB6czMyYTA1MnlwcjJmaXljdGNmIn0.Ukuvh6H-szz9m6G8cAamQQ'
-  }).addTo(map);
 
-    console.log(lat)
-    console.log(long)
-    console.log(response);
-    document.querySelector("#testText").innerHTML = response.businesses[0].name
-    document.querySelector("#testImage").setAttribute("src", response.businesses[0].image_url)
-    document.getElementById("address").textContent = response.businesses[0].location.display_address
-    document.querySelector("#cityName").textContent = response.businesses[0].location.city
-    document.getElementById('thing').value = "";
   })
   .catch(err => {
     console.error(err);
   });
 }
 
+function displayMap(lat,long) {
+  // $(mapEL).html("");
+
+  let map = L.map('map').setView([lat, long], 20);
+  
+  // if (map!=undefined) {
+  //   map.off();
+  //  map.remove(); 
+  // };
+
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoianN1YnJhaSIsImEiOiJja3piejB6czMyYTA1MnlwcjJmaXljdGNmIn0.Ukuvh6H-szz9m6G8cAamQQ'
+}).addTo(map);
+
+};
+
+function displayShop(response) {
+
+  document.querySelector("#testText").innerHTML = response.businesses[0].name
+  document.querySelector("#testImage").setAttribute("src", response.businesses[0].image_url)
+  document.getElementById("address").textContent = response.businesses[0].location.display_address
+  document.querySelector("#cityName").textContent = response.businesses[0].location.city
+  document.getElementById('thing').value = "";
+
+}
+
 document.getElementById("thingBtn").addEventListener("click", thing);
+
+
+
 // Fetch opentripmap below
 // fetch ('https://api.opentripmap.com/0.1/­en/tiles/pois­/10­/1/1.pbf&apikey=5ae2e3f221c38a28845f05b65aa94ae67c8a988c80831bd867503215') 
 //     .then(response => response.json())
