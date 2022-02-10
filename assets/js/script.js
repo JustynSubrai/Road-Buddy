75f8d0191cc288d2d872f0bac39c53a037d97482
Christine Zheng <christinezheng@Meltons-MacBook-Pro.local>
2022-02-10 14:47:05 -0800

    modal changes and location chngs

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,44 +5,44 @@ function thing() {
   fetch("https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?limit=5&location=" + document.querySelector("#thing").value, {
     "method": "GET",
     "headers": {
-      "Authorization": "Bearer W4S7TecUBgIRXsrzLF-Y1aMW4KebkqjTiWx8-IlN3jZ0tzA78-d6cNaoXAzHecWreYhXOR1akdRUWwQVTMGe8VfJhj-5c0Z9l9yGo4H0QcIGjlH9RPVNRi_45UX7YXYx" ,
+      "Authorization": "Bearer W4S7TecUBgIRXsrzLF-Y1aMW4KebkqjTiWx8-IlN3jZ0tzA78-d6cNaoXAzHecWreYhXOR1akdRUWwQVTMGe8VfJhj-5c0Z9l9yGo4H0QcIGjlH9RPVNRi_45UX7YXYx",
     }
   })
-  .then(response => {
-    return response.json();
-  })
-  .then(response => {
-    let lat = response.businesses[0].coordinates.latitude
-    let long = response.businesses[0].coordinates.longitude
-    displayMap(lat, long);
-    displayShop(response);
-    // console.log(lat)
-    // console.log(long)
-     console.log(response);
+    .then(response => {
+      return response.json();
+    })
+    .then(response => {
+      let lat = response.businesses[0].coordinates.latitude
+      let long = response.businesses[0].coordinates.longitude
+      displayMap(lat, long);
+      displayShop(response);
+      // console.log(lat)
+      // console.log(long)
+      console.log(response);
 
 
 
-  })
-  .catch(err => {
-    console.error(err);
-  });
+    })
+    .catch(err => {
+      console.error(err);
+    });
 }
 
 
-$("#thingBtn").on("click",function(event) {
-event.preventDefault();
-thing();
+$("#thingBtn").on("click", function (event) {
+  event.preventDefault();
+  thing();
 
 });
 
 // document.getElementById("thingBtn").addEventListener("click", thing);
 
-function displayMap(lat,long) {
+function displayMap(lat, long) {
 
-	document.getElementById('map-container').innerHTML = "<div id='map'></div>";
-	let map = new L.map('map');
+  document.getElementById('map-container').innerHTML = "<div id='map'></div>";
+  let map = new L.map('map');
 
-	map.setView([lat, long], 20);
+  map.setView([lat, long], 20);
 
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -51,19 +51,45 @@ function displayMap(lat,long) {
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoianN1YnJhaSIsImEiOiJja3piejB6czMyYTA1MnlwcjJmaXljdGNmIn0.Ukuvh6H-szz9m6G8cAamQQ'
-}).addTo(map);
+  }).addTo(map);
 
 };
 
+//Below is the Local Storage for getting the locations
+
+function getLocation() {
+  var arr = []
+  for (let i = 0; i < localStorage.length; i++) {
+    let previousOnes = JSON.stringify(localStorage.getItem("Location" + i))
+    if (previousOnes != '""') {
+      arr.push(localStorage.getItem("Location" + i))
+    }
+    console.log(previousOnes);
+    ;
+
+
+  }
+
+  document.getElementById("locationsPull").innerHTML = ""
+
+
+  for (let i = 0; i < arr.length; i++) {
+    document.getElementById("locationsPull").innerHTML += arr[i] + "</br>";
+  }
+
+}
+
 function setShop(response) {
-localStorage.setItem("Location" + localStorage.length, document.querySelector("#thing").value);
+  console.log("CALLED")
+  localStorage.setItem("Location" + localStorage.length, document.querySelector("#thing").value);
+  getLocation()
 }
 
 
 // Displays all the businesses in the City
 function displayShop(response) {
   var dataEl = (`<div class="columns is-mobile  is-flex-wrap-wrap is-justify-content-center is-align-content-center">`);
-  for (let i=0; i < response.businesses.length; i++) {
+  for (let i = 0; i < response.businesses.length; i++) {
     var nameEl = response.businesses[i].name;
     var imageUrlEl = response.businesses[i].image_url;
     var addressEl = response.businesses[i].location.display_address;
@@ -85,11 +111,11 @@ function displayShop(response) {
 
 
   $("#dataResponse").html(dataEl)
-    setShop();
-    document.getElementById('thing').value = "";
-  }
+  setShop();
+  document.getElementById('thing').value = "";
+}
 
-document.getElementById("thingBtn").addEventListener("click", thing);
+// document.getElementById("thingBtn").addEventListener("click", thing);
 
 
 // Fetch opentripmap below
@@ -152,19 +178,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-//Below is the Local Storage for getting the locations
 
-function getLocation() {
-  for (let i = 0; i < localStorage.length; i++) {
-    let previousOnes = JSON.stringify(localStorage.getItem("Location" + i));
-    console.log(previousOnes);
-    document.getElementById("locationsPull").innerHTML+= previousOnes + "</br>";
-   ;
-
-
-  }
-
-}
 
 getLocation();
 
